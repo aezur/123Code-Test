0a002805a3c3be626e0b5621857941cfb509e1d8
petermulligan88 <aezur20@gmail.com>
2023-05-27 23:34:26 +0100

    Removed default components.

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import PersonalityCard from '~/components/PersonalityCard.vue'
-
 export default Vue.extend({
-  components: { PersonalityCard },
   name: 'IndexPage',
 })
 </script>
