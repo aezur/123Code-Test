8627007a705210cf3e693c055b1f0b20f40e1499
petermulligan88 <aezur20@gmail.com>
2023-05-27 23:30:25 +0100

    Added default layout.

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="default-layout" class="flex flex-col items-center">
+    <Nuxt />
+  </div>
+</template>
+
+<style scoped>
+#default-layout {
+  background-color: #e5e5e5;
+  min-height: 100vh;
+  padding-top: 84px;
+}
+</style>
