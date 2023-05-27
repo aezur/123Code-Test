c368a848ae6e80027f7057da9f739b6295b1a1bb
petermulligan88 <aezur20@gmail.com>
2023-05-27 22:38:55 +0100

    Populate description field on data fetch.

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,5 @@
 import { Commit, Dispatch } from 'vuex';
+import { loremIpsum } from '~/mixins/loremIpsum';
 
 export interface State {
   students: Student[];
@@ -16,7 +17,7 @@ export const getters = {
 
 export const mutations = {
   setStudents(state: State, students: Student[]) {
-    state.students = students ;
+    state.students = students;
   }
 }
 
@@ -24,9 +25,10 @@ export const actions = {
   nuxtServerInit({ dispatch }: { dispatch: Dispatch }) {
     return dispatch('fetchStudents');
   },
-  async fetchStudents({ commit }: { commit: Commit }) {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  async fetchStudents({ commit }: { commit: Commit }, count: number = 10) {
+    const response = await fetch(`https://random-data-api.com/api/v2/users?size=${count}`);
     const data: Student[] = await response.json();
-    return commit('setStudents', data);
+    const students = data.map((student: Student) => ({ ...student, description: loremIpsum.methods.generateParagraphs(1) }));
+    return commit('setStudents', students);
   }
 }
