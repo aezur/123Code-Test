dd4254a6ca939123a6eef86be42ca40655865499
petermulligan88 <aezur20@gmail.com>
2023-05-27 23:31:06 +0100

    Added ui skeleton.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <Tutorial />
+  <personality-card />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import PersonalityCard from '~/components/PersonalityCard.vue'
 
 export default Vue.extend({
+  components: { PersonalityCard },
   name: 'IndexPage',
 })
 </script>
