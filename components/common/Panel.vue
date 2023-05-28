b27011a526b9a50baf1e176ad203102bc1708334
petermulligan88 <aezur20@gmail.com>
2023-05-28 16:39:28 +0100

    Added panel component.

---
+++ components/common/Panel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col justify-center bg-white">
+    <div class="px-10 py-2" v-if="title" >
+      <h2 class="text-5xl font-bold">{{title}}</h2>
+    </div>
+    <div class="px-10 py-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Panel',
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+h2 {
+  color: $brand;
+}
+</style>
