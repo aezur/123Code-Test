b551fa7d595764ec3949a9ed40143911a2197b9e
petermulligan88 <aezur20@gmail.com>
2023-05-28 17:18:03 +0100

    Added pill component.

---
+++ components/common/Pill.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pill rounded-2xl px-6 py-2 font-medium text-3xl bg-gray-200 color-gray-500">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Panel',
+});
+</script>
+
+<style lang="scss" scoped>
+.pill {
+  color: $bodyText;
+  background: $neutral;
+}
+</style>
