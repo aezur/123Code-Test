ee81202fca9aa3b4e4e770e5d631d273257d4857
petermulligan88 <aezur20@gmail.com>
2023-05-28 03:46:15 +0100

    Added font to layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@500;700;800&display=swap');
 #default-layout {
   background-color: #e5e5e5;
   min-height: 100vh;
