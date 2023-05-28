c30bb7824f4527e9e015a5aa7093d74d188ca361
petermulligan88 <aezur20@gmail.com>
2023-05-28 16:40:30 +0100

    Updated PersonalityCard to use penls / details.

--- components/PersonalityCard.vue
+++ components/PersonalityCard.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="personality-card" class="flex flex-col justify-center bg-white">
+
     <user-display :student="student" />
 
     <div id="description">
       <p class="text-4xl font-medium ">{{ student?.description }}</p>
     </div>
 
-    <h2 class="text-4xl font-bold text-red-200">Details</h2>
+    <panel title="Details">
+      <user-details :student="student" />
+    </panel>
 
-    <h2 class="text-4xl font-bold text-red-200">Brands</h2>
+    <panel title="Brand"></panel>
 
     <button @click="getRandomStudent">Random Student</button>
   </div>
@@ -41,15 +44,11 @@ export default Vue.extend({
   font-family: 'Catamaran', sans-serif;
   border-radius: 50px;
   #description {
-    margin: 60px 0 90px 0;
     padding: 8px 40px;
     p {
       line-height: 59px;
       color: $bodyText;
     }
-  }
-  h2 {
-    color: $brand;
   }
 }
 </style>
